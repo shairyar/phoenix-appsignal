3d17fbff589cf58643bfd93b2072029ed3530147
Shairyar Baig <shairyarbaig@gmail.com>
2020-12-09 19:57:01 +0500

    Add tagging and custom span

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -2,6 +2,25 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
+    tagging()
+    slow()
     render(conn, "index.html")
   end
+
+  defp tagging do
+    # Adding sample data example (https://docs.appsignal.com/elixir/instrumentation/tagging.html)
+    Appsignal.Span.set_sample_data(Appsignal.Tracer.current_span, "params", %{locale: "en"})
+
+    # the syntax might not be correct
+    # Appsignal.Span.set_sample_data(Appsignal.Tracer.current_span, "somekey", "somevalue")
+  end
+
+  defp slow do
+
+    # Adding spans to traces (https://docs.appsignal.com/elixir/instrumentation/instrumentation.html#add-spans-to-traces)
+    Appsignal.instrument("slow", fn ->
+      :timer.sleep(1000)
+    end)
+  end
+
 end
